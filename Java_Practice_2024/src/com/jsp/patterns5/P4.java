4d6d5c9e70a3362120407b022e38516f75d5a1fb
SriRajendraT <rajendratm120@gmail.com>
2024-04-19 14:24:42 +0530

    Patterns5 P4 updated

--- Java_Practice_2024/src/com/jsp/patterns5/P4.java
+++ Java_Practice_2024/src/com/jsp/patterns5/P4.java
@@ -4,20 +4,28 @@ public class P4 {
 
 	public static void main(String[] args) {
 		int num=5;
-		for(int row=1;row<num-1;row++) {
+		for (int row = 1; row <= num/2+1; row++) {
 			char ch='A';
-			for(int col=row;col<num-1;col++) {
+			for (int col = 1; col <= num - row; col++) {
 				System.out.print("_ ");
 			}
-			for(int col=1;col<row;col++) {
+			for (int col = 1; col <= row*2-1; col++) {
 				System.out.print(ch+" ");
 				ch++;
 			}
-			for (int col = 1; col <= row; col++) {
+			System.out.println();
+		}
+
+		for (int row = num /2 ; row >= 1; row--) {
+			char ch='A';
+			for (int col = 1; col <= num - row; col++) {
+				System.out.print("_ ");
+			}
+			for (int col = 1; col <= row*2-1; col++) {
 				System.out.print(ch+" ");
 				ch++;
 			}
-		System.out.println();
+			System.out.println();
 		}
 	}
 
