079833719106accc8f9a37aafd4dd404f915f4ec
SriRajendraT <rajendratm120@gmail.com>
2024-04-19 18:04:39 +0530

    New Pattren5 P6

---
+++ Java_Practice_2024/src/com/jsp/patterns5/P6.java
@@ -0,0 +1,22 @@
+package com.jsp.patterns5;
+
+public class P6 {
+
+	public static void main(String[] args) {
+		int num = 5;
+		for (int row = 1; row < num; row++) {
+			for (int col = row; col < num; col++) {
+				System.out.print("_ ");
+			}
+			for (char col = 'A'; col < 'A' + row; col++) {
+				System.out.print(col + " ");
+			}
+
+			for (char col = (char) ('A' + row - 2); col >= 'A'; col--) {
+				System.out.print(col + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
