3ad6866b2342c0ebe5d451f5e7df82c6ba0f9cf6
SriRajendraT <rajendratm120@gmail.com>
2024-04-22 13:26:50 +0530

    Recursion P5 update

--- Java_Practice_2024/src/com/jsp/Recursion/P5.java
+++ Java_Practice_2024/src/com/jsp/Recursion/P5.java
@@ -2,22 +2,25 @@ package com.jsp.Recursion;
 
 public class P5 {
 
-	public static void main(String[] args) {}
-//		int num=121;
-//		if(num== revNum(num,0)) {
-//			System.out.println("the given number "+num+" is palindrome");
-//		}else {
-//			System.out.println("the given number "+num+" is not palindrome");
-//		}
-//	}
-//	
-//	
-//	public static int revNum(int num,int rev) {
-//		int rem=0;
-//		rem=num%10;
-//		rev=rev*10+rem;
-//		num/=10;
-//		return num!=0?revNum(num/10, rev):rev;
-//	}
+	public static void main(String[] args) {
+		int num=1231;
+		int res=revNum(num,0);
+		if(num==res ) {
+			System.out.println("the given number "+num+" is palindrome");
+		}else {
+			System.out.println("the given number "+num+" is not palindrome");
+		}
+	}
+	
+	
+	public static int revNum(int num,int rev) {
+		if(num==0) {
+			return rev;
+		}
+		int rem=0;
+		rem=num%10;
+		rev=rev*10+rem;
+		return num!=0?revNum(num/10, rev):rev;
+	}
 
 }
