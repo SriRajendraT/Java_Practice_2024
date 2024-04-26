6fc2d0e91db2a2e27f821f60167f242b96c296b6
SriRajendraT <rajendratm120@gmail.com>
2024-04-26 20:21:06 +0530

    P14

--- Java_Practice_2024/src/com/jsp/collections/P14.java
+++ Java_Practice_2024/src/com/jsp/collections/P14.java
@@ -1,20 +1,27 @@
 package com.jsp.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class P14 {
 
 	public static void main(String[] args) {
-		
+		ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Apple");
+        arrayList.add("Banana");
+        arrayList.add("Orange");
+        arrayList.add("Mango");	
+        
+        System.out.println("Original ArrayList: " + arrayList);
+        
+        arrayList.set(1, "Grapes");
+        
+        System.out.println("ArrayList after replacing an element: " + arrayList);
+        
+        Collections.replaceAll(arrayList, "Orange", "Pineapple");
+        
+        System.out.println("ArrayList after replacing all occurrences of an element: " + arrayList);
 	}
 	
-	public static <T> ArrayList<T> replaceElement(ArrayList<T> lists,Integer search,Integer replace){
-		
-		for(var list:lists) {
-			if(list==search) {
-				
-			}
-		}
-		return null;
-	}
+	
 }
