91dc3b32d9faadc187da14db6e003888947fe381
SriRajendraT <rajendratm120@gmail.com>
2024-04-29 16:46:45 +0530

    String P10 updated

--- Java_Practice_2024/src/com/jsp/strings/P10.java
+++ Java_Practice_2024/src/com/jsp/strings/P10.java
@@ -4,19 +4,19 @@ public class P10 {
 
 	public static void main(String[] args) {
 		String str = "appa";
-		int count=countCharacter(str);
-		System.out.println("number of character present in "+str+" is "+count);
+		int count = countCharacter(str);
+		System.out.println("number of character present in " + str + " is " + count);
 	}
-	
+
 	public static int countCharacter(String str) {
-		str=str.toLowerCase();
-		int count=0;
-		
-		for(char ch:str.toCharArray()) {
+		str = str.toLowerCase();
+		int count = 0;
+
+		for (char ch : str.toCharArray()) {
 			count++;
-			System.out.print(ch+" ");
+			System.out.print(ch + " ");
 		}
-		
+
 		return count;
 	}
 }
